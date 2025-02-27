6e7bd307baece509928ada4523640c44e86f17d3
Sabare vijay <sabarevijay2001@gmail.com>
2025-02-27 14:14:28 +0530

    vercel config in backend-.env correction2

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,8 @@ dotenv.config()
 const app = express()
 const PORT=process.env.PORT || 8000
 const corsOptions={
-    origin:[process.env.FRONTEND_URL],
+    origin:'https://blogg-client-six.vercel.app/',
+    // origin:[process.env.FRONTEND_URL],
     credentials:true
 }
 
